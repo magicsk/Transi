c0358bc9386cc9aabbe3e1c13a3a43731ea40106
magicsk <magicsk@magicsk.eu>
2022-07-18 22:26:01 +0200

    build: disabled minify for debug build build: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,14 +35,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
