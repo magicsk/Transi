94d86985d7d0db10907c031e11049fdd4ea67a8b
magicsk <magicsk@magicsk.eu>
2022-07-18 22:25:04 +0200

    ref: replaced old parcelize package

--- app/src/main/java/eu/magicsk/transi/data/models/SelectedTrip.kt
+++ app/src/main/java/eu/magicsk/transi/data/models/SelectedTrip.kt
@@ -1,7 +1,7 @@
 package eu.magicsk.transi.data.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
 @Parcelize
--- app/src/main/java/eu/magicsk/transi/data/remote/responses/PlatformLabels.kt
+++ app/src/main/java/eu/magicsk/transi/data/remote/responses/PlatformLabels.kt
@@ -1,7 +1,7 @@
 package eu.magicsk.transi.data.remote.responses
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class PlatformLabels : ArrayList<PlatformLabel>(), Parcelable
